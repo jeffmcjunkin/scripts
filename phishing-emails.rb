77dc54d0e38464077c05f65839e10d7a0a76c96c
Jeff McJunkin <jeff.mcjunkin@gmail.com>
2013-06-02 19:40:33 -0700

    Changed to ruby-gmail, added most functionality.

--- phishing-emails.rb
+++ phishing-emails.rb
@@ -2,29 +2,33 @@
 # Some code blatantly stolen from  http://lindsaar.net/2010/3/15/how_to_use_mail_and_actionmailer_3_with_gmail_smtp
 # By Jeff McJunkin (jeff.mcjunkin@gmail.com)
 
-require 'mail'
-
-options = { :address              => "smtp.gmail.com",
-            :port                 => 587,
-            :domain               => ENV['GMAIL_DOMAIN'],
-            :user_name            => ENV['GMAIL_USERNAME'],
-            :password             => ENV['GMAIL_PASSWORD'],
-            :authentication       => 'plain',
-            :enable_starttls_auto => true  }
+require 'gmail'
+require 'base64'
 
 # Note: I'm using environment variables here.
 # To use hard-coded variables instead (don't commit them to Github!)
 # just change the lines to something like 
-#           :password             => '<password>',
+# gmail = Gmail.new("username@domain.com", "password")
 
+gmail = Gmail.new(ENV['GMAIL_USERNAME'],ENV['GMAIL_PASSWORD'])
 
-Mail.defaults do
-  delivery_method :smtp, options
+templateBody = File.read('/tmp/ups.txt')
+
+# Build the tracking URL
+baseDomain = "www.upsdeliverytracking.com/index.html"
+targetEmailAddress = "jeff.mcjunkin@gmail.com"
+url = baseDomain + "?id=" + Base64.encode64(targetEmailAddress)
+
+# Search and replace the literal string "__URL__" with the combination of the domain name 
+# and the base64'd email address of the recipient (in an ?id=<base64encodedemail> parameter)
+emailBody = templateBody.gsub(/__URL__/, url)
+
+gmail.deliver do
+  to "jeff.mcjunkin@gmail.com"
+  subject "UPS Ship Notification"
+  html_part do
+    body emailBody
+  end
 end
 
-Mail.deliver do
-       to 'target@companyname.com'
-     from ENV['GMAIL_USERNAME']
-  subject 'testing sendmail'
-     body 'testing sendmail'
-end
+gmail.logout
